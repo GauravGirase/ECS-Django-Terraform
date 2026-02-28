ba3bb6fa5f2f6e400924b8206123d832adf2c273
Gaurav <gaurav.girase111@gmail.com>
2026-02-28 22:37:16 +0530

    added appautoscaling

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,3 +1,7 @@
 output "alb_target_group_arn" {
   value = aws_alb.ecs-alb.arn
 }
+
+output "alb_hostname" {
+  value = aws_alb.ecs-alb.dns_name
+}
---
+++ modules/asg/variables.tf
@@ -0,0 +1,5 @@
+variable "ecs_cluster_name" {}
+variable "env" {}
+variable "autoscale_max" {} #10
+variable "autoscale_min" {} # 1
+variable "ecs_service_name" {}
