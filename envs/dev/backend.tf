5bd4b81fc4cc3e3ffcb7cc74b385c91aeba52b89
Gaurav <gaurav.girase111@gmail.com>
2026-03-01 00:27:14 +0530

    testing on ec2

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-    backend "s3" {
-        name = "terrformstate-ecs-django"
-        key = "dev/terraform.tfstate"
-        encrypt = true
-        lock = true
-        region = var.region
-    }
-}
+# terraform {
+#     backend "s3" {
+#         name = "terrformstate-ecs-django"
+#         key = "dev/terraform.tfstate"
+#         encrypt = true
+#         lock = true
+#         region = var.region
+#     }
+# }
