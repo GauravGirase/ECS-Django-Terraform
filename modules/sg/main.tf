14cb87dd8e3e6ac1961eb6db00d7032f955a5611
Gaurav <gaurav.girase111@gmail.com>
2026-03-10 19:59:52 +0530

    change target group port to match with container port 8000

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -35,8 +35,8 @@ resource "aws_security_group" "ecs-sg" {
 
     # We can restric to the specific ports later
     ingress {
-        from_port = 8000
-        to_port = 8000
+        from_port = 80
+        to_port = 80
         protocol = "tcp"
         security_groups = [aws_security_group.alb_sg.id]
     }
