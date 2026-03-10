d94902ccb0f2f138cc24ee28e3c48f95c9ea96b6
Gaurav <gaurav.girase111@gmail.com>
2026-03-10 19:56:05 +0530

    updated ingress port to 8000 from 0

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -35,8 +35,8 @@ resource "aws_security_group" "ecs-sg" {
 
     # We can restric to the specific ports later
     ingress {
-        from_port = 0
-        to_port = 0
+        from_port = 8000
+        to_port = 8000
         protocol = "tcp"
         security_groups = [aws_security_group.alb_sg.id]
     }
