e01ad32d646933364176619bee02e31e8d578e0f
Gaurav Rajendra Girase <78299809+GauravGirase@users.noreply.github.com>
2026-03-11 09:24:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,378 @@
+variable "region" {
+  description = "The AWS region to create resources in."
+  default     = "ap-south-1"
+}
+
+# networking
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+  default     = "10.0.4.0/24"
+}
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-west-1b", "us-west-1c"]
+}
+
+# load balancer
+
+variable "health_check_path" {
+  description = "Health check path for the default target group"
+  default     = "/ping/"
+}
+
+# ecs
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  default     = "production"
+}
+# logs
+variable "log_retention_in_days" {
+  default = 30
+}
+# ecs
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  default     = "production"
+}
+
+variable "docker_image_url_django" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "<AWS_ACCOUNT_ID>.dkr.ecr.us-west-1.amazonaws.com/django-app:latest"
+}
+
+variable "app_count" {
+  description = "Number of Docker containers to run"
+  default     = 2
+}
+
+variable "fargate_cpu" {
+  description = "Amount of CPU for Fargate task. E.g., '256' (.25 vCPU)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Amount of memory for Fargate task. E.g., '512' (0.5GB)"
+  default     = "512"
+}
+# ECS service auto scaling
+
+variable "autoscale_min" {
+  description = "Minimum autoscale (number of tasks)"
+  default     = "1"
+}
+
+variable "autoscale_max" {
+  description = "Maximum autoscale (number of tasks)"
+  default     = "10"
+}
+
+variable "autoscale_desired" {
+  description = "Desired number of tasks to run initially"
+  default     = "4"
+}
+provider "aws" {
+  region = var.region
+}
+
+# Production VPC
+resource "aws_vpc" "production-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+# Public subnets
+resource "aws_subnet" "public-subnet-1" {
+  cidr_block        = var.public_subnet_1_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = var.availability_zones[0]
+}
+resource "aws_subnet" "public-subnet-2" {
+  cidr_block        = var.public_subnet_2_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = var.availability_zones[1]
+}
+
+# Private subnets
+resource "aws_subnet" "private-subnet-1" {
+  cidr_block        = var.private_subnet_1_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = var.availability_zones[0]
+}
+resource "aws_subnet" "private-subnet-2" {
+  cidr_block        = var.private_subnet_2_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = var.availability_zones[1]
+}
+
+# Route tables for the subnets
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.production-vpc.id
+}
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.production-vpc.id
+}
+
+# Associate the newly created route tables to the subnets
+resource "aws_route_table_association" "public-route-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+resource "aws_route_table_association" "public-route-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+resource "aws_route_table_association" "private-route-1-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
+resource "aws_route_table_association" "private-route-2-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
+}
+
+# Elastic IP
+resource "aws_eip" "elastic-ip-for-nat-gw" {
+  domain                       = "vpc"
+  associate_with_private_ip = "10.0.0.5"
+  depends_on                = [aws_internet_gateway.production-igw]
+}
+
+# NAT gateway
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.elastic-ip-for-nat-gw.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+  depends_on    = [aws_eip.elastic-ip-for-nat-gw]
+}
+resource "aws_route" "nat-gw-route" {
+  route_table_id         = aws_route_table.private-route-table.id
+  nat_gateway_id         = aws_nat_gateway.nat-gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+# Internet Gateway for the public subnet
+resource "aws_internet_gateway" "production-igw" {
+  vpc_id = aws_vpc.production-vpc.id
+}
+
+# Route the public subnet traffic through the Internet Gateway
+resource "aws_route" "public-internet-igw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.production-igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+# ALB Security Group (Traffic Internet -> ALB)
+resource "aws_security_group" "load-balancer" {
+  name        = "load_balancer_security_group"
+  description = "Controls access to the ALB"
+  vpc_id      = aws_vpc.production-vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS Fargate Security group (traffic ALB -> ECS Fargate Tasks)
+resource "aws_security_group" "ecs-fargate" {
+  name        = "ecs_fargate_security_group"
+  description = "Allows inbound access from the ALB only"
+  vpc_id      = aws_vpc.production-vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.load-balancer.id]
+  }
+
+  # No SSH ingress rule since Fargate tasks are abstracted and not directly accessible via SSH
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+# Production Load Balancer
+resource "aws_lb" "production" {
+  name               = "${var.ecs_cluster_name}-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.load-balancer.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+}
+
+# Target group for ECS Fargate
+resource "aws_alb_target_group" "default-target-group" {
+  name     = "${var.ecs_cluster_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.production-vpc.id
+  target_type = "ip"
+
+  health_check {
+    path                = var.health_check_path
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 5
+    matcher             = "200"
+  }
+}
+
+# Listener (redirects traffic from the load balancer to the target group)
+resource "aws_alb_listener" "ecs-alb-http-listener" {
+  load_balancer_arn = aws_lb.production.id
+  port              = "80"
+  protocol          = "HTTP"
+  depends_on        = [aws_alb_target_group.default-target-group]
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+  }
+}
+resource "aws_iam_role" "ecs-task-execution-role" {
+  name               = "ecs_task_execution_role_prod"
+  assume_role_policy = file("policies/ecs-role.json")
+}
+
+resource "aws_iam_role_policy" "ecs-task-execution-role-policy" {
+  name   = "ecs_task_execution_role_policy"
+  policy = file("policies/ecs-task-execution-policy.json")
+  role   = aws_iam_role.ecs-task-execution-role.id
+}
+
+resource "aws_iam_role" "ecs-service-role" {
+  name               = "ecs_service_role_prod"
+  assume_role_policy = file("policies/ecs-role.json")
+}
+
+resource "aws_iam_role_policy" "ecs-service-role-policy" {
+  name   = "ecs_service_role_policy"
+  policy = file("policies/ecs-service-role-policy.json")
+  role   = aws_iam_role.ecs-service-role.id
+}
+resource "aws_cloudwatch_log_group" "django-log-group" {
+  name              = "/ecs/django-app"
+  retention_in_days = var.log_retention_in_days
+}
+resource "aws_ecs_cluster" "production" {
+  name = "${var.ecs_cluster_name}-cluster"
+}
+
+data "template_file" "app" {
+  template = file("templates/django_app.json.tpl")
+
+  vars = {
+    docker_image_url_django = var.docker_image_url_django
+    region                  = var.region
+  }
+}
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "django-app"
+  network_mode             = "awsvpc" # Required for Fargate
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "${var.fargate_cpu}"
+  memory                   = "${var.fargate_memory}"
+  execution_role_arn       = aws_iam_role.ecs-task-execution-role.arn
+  task_role_arn            = aws_iam_role.ecs-task-execution-role.arn
+  container_definitions    = data.template_file.app.rendered
+}
+
+
+resource "aws_ecs_service" "production" {
+  name            = "${var.ecs_cluster_name}-service"
+  cluster         = aws_ecs_cluster.production.id
+  task_definition = aws_ecs_task_definition.app.arn
+  launch_type     = "FARGATE"
+  desired_count   = var.app_count
+  network_configuration {
+    subnets          = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+    security_groups  = [aws_security_group.ecs-fargate.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+    container_name   = "django-app"
+    container_port   = 8000
+  }
+}
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = var.autoscale_max
+  min_capacity       = var.autoscale_min
+  resource_id        = "service/${aws_ecs_cluster.production.name}/${aws_ecs_service.production.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "ecs-auto-scaling-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 75
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
+  }
+}
+output "alb_hostname" {
+  value = aws_lb.production.dns_name
+}
+
+output "ecs_task_execution_role_arn" {
+    value = aws_iam_role.ecs-task-execution-role.arn
+    description = "ARN for the ECS Task Execution Role"
+}
+
+output "subnets" {
+  value = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+}
+
+output "security_group" {
+  value = aws_security_group.ecs-fargate.id
+}
